bb4e26999f27a482ec125c5c8ea55045de5bc261
masum-khondhoker-efaz <efazkh@gmail.com>
2025-08-21 18:04:39 +0600

    Feed system

--- dist/app/modules/favoriteFeed/favoriteFeed.routes.js
+++ dist/app/modules/favoriteFeed/favoriteFeed.routes.js
@@ -7,14 +7,15 @@ exports.favoriteFeedRoutes = void 0;
 const express_1 = __importDefault(require("express"));
 const auth_1 = __importDefault(require("../../middlewares/auth"));
 const favoriteFeed_controller_1 = require("./favoriteFeed.controller");
+const client_1 = require("@prisma/client");
 const router = express_1.default.Router();
-router.post('/', (0, auth_1.default)(), 
+router.post('/', (0, auth_1.default)(client_1.UserRoleEnum.CUSTOMER, client_1.UserRoleEnum.SALOON_OWNER, client_1.UserRoleEnum.BARBER), 
 //   validateRequest(favoriteFeedValidation.createSchema),
 favoriteFeed_controller_1.favoriteFeedController.createFavoriteFeed);
-router.get('/', (0, auth_1.default)(), favoriteFeed_controller_1.favoriteFeedController.getFavoriteFeedList);
-router.get('/:id', (0, auth_1.default)(), favoriteFeed_controller_1.favoriteFeedController.getFavoriteFeedById);
-router.put('/:id', (0, auth_1.default)(), 
+router.get('/', (0, auth_1.default)(client_1.UserRoleEnum.CUSTOMER, client_1.UserRoleEnum.SALOON_OWNER, client_1.UserRoleEnum.BARBER), favoriteFeed_controller_1.favoriteFeedController.getFavoriteFeedList);
+router.get('/:id', (0, auth_1.default)(client_1.UserRoleEnum.CUSTOMER, client_1.UserRoleEnum.SALOON_OWNER, client_1.UserRoleEnum.BARBER), favoriteFeed_controller_1.favoriteFeedController.getFavoriteFeedById);
+router.put('/:id', (0, auth_1.default)(client_1.UserRoleEnum.CUSTOMER, client_1.UserRoleEnum.SALOON_OWNER, client_1.UserRoleEnum.BARBER), 
 //   validateRequest(favoriteFeedValidation.updateSchema),
 favoriteFeed_controller_1.favoriteFeedController.updateFavoriteFeed);
-router.delete('/:id', (0, auth_1.default)(), favoriteFeed_controller_1.favoriteFeedController.deleteFavoriteFeed);
+router.delete('/:id', (0, auth_1.default)(client_1.UserRoleEnum.CUSTOMER, client_1.UserRoleEnum.SALOON_OWNER, client_1.UserRoleEnum.BARBER), favoriteFeed_controller_1.favoriteFeedController.deleteFavoriteFeed);
 exports.favoriteFeedRoutes = router;
--- src/app/modules/favoriteFeed/favoriteFeed.routes.ts
+++ src/app/modules/favoriteFeed/favoriteFeed.routes.ts
@@ -3,27 +3,40 @@ import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { favoriteFeedController } from './favoriteFeed.controller';
 import { favoriteFeedValidation } from './favoriteFeed.validation';
+import { UserRoleEnum } from '@prisma/client';
 
 const router = express.Router();
 
 router.post(
   '/',
-  auth(),
-//   validateRequest(favoriteFeedValidation.createSchema),
+  auth(UserRoleEnum.CUSTOMER, UserRoleEnum.SALOON_OWNER, UserRoleEnum.BARBER),
+  //   validateRequest(favoriteFeedValidation.createSchema),
   favoriteFeedController.createFavoriteFeed,
 );
 
-router.get('/', auth(), favoriteFeedController.getFavoriteFeedList);
+router.get(
+  '/',
+  auth(UserRoleEnum.CUSTOMER, UserRoleEnum.SALOON_OWNER, UserRoleEnum.BARBER),
+  favoriteFeedController.getFavoriteFeedList,
+);
 
-router.get('/:id', auth(), favoriteFeedController.getFavoriteFeedById);
+router.get(
+  '/:id',
+  auth(UserRoleEnum.CUSTOMER, UserRoleEnum.SALOON_OWNER, UserRoleEnum.BARBER),
+  favoriteFeedController.getFavoriteFeedById,
+);
 
 router.put(
   '/:id',
-  auth(),
-//   validateRequest(favoriteFeedValidation.updateSchema),
+  auth(UserRoleEnum.CUSTOMER, UserRoleEnum.SALOON_OWNER, UserRoleEnum.BARBER),
+  //   validateRequest(favoriteFeedValidation.updateSchema),
   favoriteFeedController.updateFavoriteFeed,
 );
 
-router.delete('/:id', auth(), favoriteFeedController.deleteFavoriteFeed);
+router.delete(
+  '/:id',
+  auth(UserRoleEnum.CUSTOMER, UserRoleEnum.SALOON_OWNER, UserRoleEnum.BARBER),
+  favoriteFeedController.deleteFavoriteFeed,
+);
 
 export const favoriteFeedRoutes = router;
