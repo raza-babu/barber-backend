6dbd5184ef5d9aafe23c077e6cd6635ccdc6eafe
masum-khondhoker-efaz <efazkh@gmail.com>
2026-04-09 09:36:31 +0600

    notification added

--- src/app/utils/serviceAccount.ts
+++ src/app/utils/serviceAccount.ts
@@ -12,5 +12,4 @@ export const serviceAccount = {
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40barber-9cf6a.iam.gserviceaccount.com",
   "universe_domain": "googleapis.com"
-
 }
